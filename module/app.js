c5af7a2ab35585ee78018d4853e6929281b7600e
chaudhuree <chaudhuree00@gmail.com>
2022-08-22 07:32:01 +0600

    docs: alternative way to export module

--- module/app.js
+++ module/app.js
@@ -1,6 +1,11 @@
 const name=require('./names')
 const sayHi=require('./utils')
 
+
+const data =require('./alternative-export')
+console.log("🚀 ~ file: app.js ~ line 4 ~ data", data)
+
+
 console.log("🚀 ~ file: app.js ~ line 2 ~ name", name)
 console.log("🚀 ~ file: app.js ~ line 2 ~ sayHi", sayHi)
 
