aee2a40062d90571960b9262608f6d9f2229f2cb
chaudhuree <chaudhuree00@gmail.com>
2022-08-22 07:24:57 +0600

    docs:module

---
+++ module/app.js
@@ -0,0 +1,8 @@
+const name=require('./names')
+const sayHi=require('./utils')
+
+console.log("🚀 ~ file: app.js ~ line 2 ~ name", name)
+console.log("🚀 ~ file: app.js ~ line 2 ~ sayHi", sayHi)
+
+sayHi(name.sohan)
+sayHi(name.chaudhuree)
---
+++ module/names.js
@@ -0,0 +1,9 @@
+// local
+const SECRET='it is a secret'
+
+// public
+const sohan='sohan'
+const chaudhuree = 'chaudhuree'
+
+module.exports = {
+  sohan,chaudhuree}
