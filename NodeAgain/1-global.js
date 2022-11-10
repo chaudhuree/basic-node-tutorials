d0667e2d8b7f30353825c0f49d0a6506a2f54d40
chaudhuree <chaudhuree00@gmail.com>
2022-11-10 22:03:06 +0600

    docs: globals

---
+++ NodeAgain/1-global.js
@@ -0,0 +1,8 @@
+// note: this are common globals 
+// __dirname
+// __filename
+// module
+// require
+// process
+console.log("directory name(dirname): "+__dirname);
+console.log("filename name(filename): "+__filename);
