0d8090c88b0c988c7fb3c22660cb6333d057dd13
chaudhuree <chaudhuree00@gmail.com>
2022-11-14 21:28:55 +0600

    docs: http module

--- NodeAgain/app.js
+++ NodeAgain/app.js
@@ -1,17 +1,6 @@
-const http = require('http');
+const {readFile,writeFile}=require('fs')
+const utils = require('uitls')
 
-const server = http.createServer(function (req, res) {
-  if (req.url === '/') {
-    // res.write()
-    res.end('hi chaudhuree. i am from node server & this is home')
-  }
-  else if (req.url==='/about'){
-    res.end('this is a simple description about us..')
-  }else{
-    res.end(`<h1>Ooops..</h1>
-  <p>there has an error to load this page</p>
-  <a href="/">back to home</a>`)
-  }
-})
+const readfileAsync=utils.promisify(readFile)
+const writefileAsync=utils.promisify(writeFile)
 
-server.listen(5000)
