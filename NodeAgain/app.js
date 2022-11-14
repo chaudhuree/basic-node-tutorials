17ab6e873033c5541e36df2de178d5a26066c957
chaudhuree <chaudhuree00@gmail.com>
2022-11-14 21:37:14 +0600

    docs: async awaite read write wtih util.promisify

--- NodeAgain/app.js
+++ NodeAgain/app.js
@@ -1,6 +1,22 @@
 const {readFile,writeFile}=require('fs')
-const utils = require('uitls')
+const util = require('util')
 
-const readfileAsync=utils.promisify(readFile)
-const writefileAsync=utils.promisify(writeFile)
+const readfileAsync=util.promisify(readFile)
+const writefileAsync=util.promisify(writeFile)
 
+const readWrite=async () =>{
+  try {
+    // read file
+    const first = await readfileAsync('./content/first.txt','utf8')
+    const second = await readfileAsync('./content/second.txt','utf8')
+    console.log("▶ ➡ file: app.js ➡ line 9 ➡ readWrite ➡ first", first);
+    console.log("▶ ➡ file: app.js ➡ line 11 ➡ readWrite ➡ second", second);
+
+    // write file
+    await writefileAsync('./content/readWriteFileAsync.txt',`awesome the text is: ${first} and ${second}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+readWrite()
